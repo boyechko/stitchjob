03b0bfdcf4aeeffd98ca2701705b995caa8d0363
Richard Boyechko <code@diachronic.net>
2025-07-10 12:30:59 -0700

    Refactor(letter): Extract self-contained functions from main()

--- stitch_letter.py
+++ stitch_letter.py
@@ -7,11 +7,37 @@ import frontmatter
 import xml.etree.ElementTree as ET
 import stitch_resume
 
-def compile_pdf(tex_path: Path):
+def get_contact_info(resume_path: Path) -> stitch_resume.Contact:
+    """Return contact information parsed from XML resume file."""
+    try:
+        resume = stitch_resume.Resume(resume_path)
+        return resume.contact
+    except FileNotFoundError as err:
+        print(f"\nError: XML resume file '{resume_path}' not found")
+        sys.exit(1)
+    except ET.ParseError as err:
+        print(f"\nError: Cannot parse '{resume_path}': {err}")
+        sys.exit(1)
+
+def parse_input_file(input_path: Path) -> dict[str, str]:
+    """Parse the input file, returning a dictionary of its content."""
+    try:
+        parsed = frontmatter.load(input_path)
+        letter = {}; letter['body'] = stitch_resume.LaTeX.escape(parsed.content)
+        for key, val in parsed.metadata.items():
+            letter[key] = stitch_resume.LaTeX.escape(val)
+        return letter
+    except FileNotFoundError:
+        print(f"\nError: Input Markdown file '{input_path}' not found")
+        sys.exit(1)
+    except Exception as err:
+        print(f"\nError: {err}")
+        sys.exit(1)
+
+def compile_pdf(tex_path: Path) -> None:
     """Compile the given LaTeX file into a PDF using pdflatex."""
     tex_path = tex_path.resolve()
 
-    print("Compiling PDF file...", end='')
     try:
         result = subprocess.run(
             ["pdflatex", "-interaction=nonstopmode", f"-output-directory={tex_path.parent}", tex_path.name],
@@ -20,7 +46,7 @@ def compile_pdf(tex_path: Path):
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        print("Done")
+        return
     except subprocess.CalledProcessError as e:
         print("\nError: ")
         print(e.stdout.decode(errors="replace"))
@@ -64,7 +90,8 @@ def signature_image(args: argparse.Namespace, metadata: dict[str, str]) -> Path 
     except ValueError:
         return sig_path
 
-def main():
+def setup_argument_parser() -> argparse.ArgumentParser:
+    """Setup the parser and return it, but don't parse the arguments."""
     parser = argparse.ArgumentParser(description="Generate LaTeX cover letter from Markdown")
     parser.add_argument("input", nargs="?",
                         default="letter/letter.md",
@@ -81,7 +108,10 @@ def main():
                         help="Output LaTeX file (default: <input>.tex)")
     parser.add_argument("-p", "--pdf", action="store_true",
                         help="Compile the .tex file to PDF using pdflatex")
-    args = parser.parse_args()
+    return parser
+
+def main():
+    args = setup_argument_parser().parse_args()
     input_path = Path(args.input).resolve()
     resume_path = Path(args.resume)
 
@@ -92,39 +122,16 @@ def main():
         output_path = Path(args.output)
 
     print(f"Getting contact information...", end='')
-    try:
-        resume = stitch_resume.Resume(resume_path)
-        contact = resume.contact
-        print("Done")
-    except FileNotFoundError as err:
-        print(f"\nError: XML resume file '{args.resume}' not found")
-        sys.exit(1)
-    except ET.ParseError as err:
-        print(f"\nError: Cannot parse '{resume_path}': {err}")
-        sys.exit(1)
+    contact = get_contact_info(resume_path)
+    print("Done")
 
     print(f"Parsing Markdown input file...", end='')
-    try:
-        parsed = frontmatter.load(input_path)
-        letter = {}; letter['body'] = stitch_resume.LaTeX.escape(parsed.content)
-        for key, val in parsed.metadata.items():
-            letter[key] = stitch_resume.LaTeX.escape(val)
-        print("Done")
-    except FileNotFoundError:
-        print(f"Error: Input Markdown file '{input_path}' not found")
-        sys.exit(1)
-    except Exception as err:
-        print(f"\nError: {err}")
-        sys.exit(1)
-
-    try:
-        template = Template(filename='letter/template.mako')
-    except Exception as err:
-        print(f"Error: {err}")
-        sys.exit(1)
+    letter = parse_input_file(input_path)
+    print("Done")
 
     print(f"Stitching LaTeX file...", end='')
-    sig_path = signature_image(args, parsed.metadata)
+    template = Template(filename='letter/template.mako')
+    sig_path = signature_image(args, letter)
     try:
         output_path.parent.mkdir(parents=True, exist_ok=True)
         with open(output_path, 'w') as file:
@@ -139,7 +146,9 @@ def main():
         sys.exit(1)
 
     if args.pdf:
+        print("Compiling PDF file...", end='')
         compile_pdf(output_path)
+        print("Done")
 
 if __name__ == "__main__":
     main()
